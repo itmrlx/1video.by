13001945e00d693d25e7c467f14d9948f4fe336b
itmrlx <itmrlx@gmail.com>
2016-11-03 12:15:56 +0300

    metrika

--- footer.php
+++ footer.php
@@ -56,6 +56,7 @@
 	<script src="<?php bloginfo('template_url'); ?>/js/min/slick-min.js"></script>
 	<script src="<?php bloginfo('template_url'); ?>/js/min/jquery.navgoco-min.js"></script>
 	<script src="<?php bloginfo('template_url'); ?>/js/min/script-min.js"></script>
+	<!-- Yandex.Metrika counter --> <script type="text/javascript"> (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter40455055 = new Ya.Metrika({ id:40455055, clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true, trackHash:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "https://mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks"); </script> <noscript><div><img src="https://mc.yandex.ru/watch/40455055" style="position:absolute; left:-9999px;" alt="" /></div></noscript> <!-- /Yandex.Metrika counter -->
 	<?php wp_footer(); ?>
 </body>
 </html>
